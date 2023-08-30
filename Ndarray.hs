63efe95c8ffffc8597a7f9816bf95b82ec9eef6d
deadManAlive <alfarrakhan@gmail.com>
2023-08-30 11:36:53 +0700

    didn't know haskell add accessor automatically

--- Ndarray.hs
+++ Ndarray.hs
@@ -2,9 +2,9 @@ module Ndarray where
 
     -- generic n-dimensional array data structure with data, shape, and stride information
     data Tensor a where
-        Tensor :: Num a => { arr :: [a]
-                           , shape :: [Int]
-                           , stride :: [Int]
+        Tensor :: Num a => { tarr :: [a]
+                           , tshape :: [Int]
+                           , tstride :: [Int]
                            } -> Tensor a
 
     -- constructors and public methods
@@ -22,24 +22,15 @@ module Ndarray where
 
     getItem:: Tensor a -> [Int] -> Maybe a
     getItem t idx
-        | offset >= getLen t = Nothing
-        | otherwise = Just ((getData t) !! offset)
+        | offset >= tlen t = Nothing
+        | otherwise = Just ((tarr t) !! offset)
         where
-            offset = sum (zipWith (*) idx (getStride t))
+            offset = sum (zipWith (*) idx (tstride t))
 
     -- primitive methods (for debug & internal use)
     -- TODO: export only public functions above
-    getData :: Tensor a -> [a]
-    getData (Tensor arr _ _) = arr
-
-    getLen :: Tensor a -> Int
-    getLen (Tensor arr _ _) = length arr
-
-    getShape :: Tensor a -> [Int]
-    getShape (Tensor _ shape _) = shape
-
-    getStride :: Tensor a -> [Int]
-    getStride (Tensor _ _ stride) = stride
+    tlen :: Tensor a -> Int
+    tlen (Tensor arr _ _) = length arr
 
     shapeToStride :: [Int] -> [Int]
     shapeToStride shape = map calculateStride [0..length shape - 1]
@@ -47,13 +38,13 @@ module Ndarray where
             calculateStride arr = product (drop (arr + 1) shape)
 
     indexToOffset :: Tensor a -> [Int] -> Int
-    indexToOffset t idx = sum (zipWith (*) idx (getStride t))
+    indexToOffset t idx = sum (zipWith (*) idx (tstride t))
 
     printTensor :: (Show a) => Tensor a -> IO ()
     printTensor t = do
-        print (getLen t)
-        print (getData t)
-        print (getShape t)
-        print (getStride t)
+        print (tlen t)
+        print (tarr t)
+        print (tshape t)
+        print (tstride t)
 
     
