02863aa39b2740d0e69598bd7a1cc5d3af567267
deadManAlive <alfarrakhan@gmail.com>
2023-09-10 00:42:34 +0700

    make main without do

--- Main.hs
+++ Main.hs
@@ -1,11 +1,9 @@
 import Ndarray
 
 main :: IO ()
-main = do
-    let m = mkFromList [1, 2, 3, 4, 5, 6, 7, 8] [2, 2, 2]
-    let index = [0, 1, 1] in
-        case m of
-            Nothing -> print "Failed in generating tensor"
-            Just t -> do
-                printTensor t
-                print (indexToOffset t index)
+main =
+  print mt
+    >> print (indexToOffset <$> mt <*> Just offset)
+  where
+    mt = mkFromList [1, 2, 3, 4, 5, 6, 7, 8] [2, 2, 2]
+    offset = [0, 0, 1]
